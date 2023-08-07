cb601d760b2e19092eff37394f2742e87ee047d0
NovemberFalls <novaworld7@gmail.com>
2023-08-07 04:38:41 -0400

    Stable

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,47 +1,18 @@
 from rest_framework.views import APIView
-from rest_framework.views import Response
-from rest_framework import status
-
-from profiles_api import serializers
+from rest_framework.response import Response
 
 
 class HelloApiView(APIView):
     """Test API View"""
 
-    serializer_class = serializers.HelloSerializer
-    
-    def get(self, request, format= None):
-        """Returns a list of APIview features"""
-        an_apiview=[
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
-            'Is similar to a tradional Django View',
-            'Gives you the most control over your application logic',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
             'Is mapped manually to URLs',
         ]
-        
-        return Response({'message': "Hello!",
-                         'an_apiview': an_apiview})
-        
-    def post(self, request):
-        """Create a hello message with our name"""
-        serializers = self.serializer_class(data=request.data)
-        
-        if serializers.is_valid():
-            name = serializers.validated_data.get('name')
-            message = f"Hello {name}"
-            return Response({'message':message})
-        else:
-            return Response(serializers.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-            
-    def put(self, request, pk=None):
-        """Handle updating an object"""
-        return Response({'method':'PUT'})
-    
-    def patch(self, request, pk=None):
-        """Handle a partial update of an object"""
-        return Response({'method':'PATCH'})
-    
-    def delete(self, request, pk=None):
-        """Delete an object"""
-        return Response({'method':'DELETE'})
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
